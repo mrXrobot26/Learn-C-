1e88d5bc3193a1e5bcf88d25ed3ee5459e660f07
Abdo Elbessa <zezoelbessa10@egyeagles.com>
2023-10-24 21:18:16 +0300

    1010

--- Learn C#/Program.cs
+++ Learn C#/Program.cs
@@ -8,78 +8,78 @@ namespace Learn_Sharp
         static void Main(string[] args)
         {
             #region var
-            // int , string , double , datatime , bool , char
-            //int x = 0;
-            //string str = Console.ReadLine();
-            //double  d = 15555555555.22222d;
-            //DateTime dateTime = DateTime.Now;
-            //bool t = true;
-            //bool f = false;
-            //char ch = 'a';
+           //int , string , double , datatime, bool , char
+            int x = 0;
+            string str = Console.ReadLine();
+            double d = 15555555555.22222d;
+            DateTime dateTime = DateTime.Now;
+            bool t = true;
+            bool f = false;
+            char ch = 'a';
 
             #endregion
 
             #region console
-            // cw , cr
+           // cw , cr
 
-            //Console.WriteLine($"Hello! {str} {dateTime}");
+            Console.WriteLine($"Hello! {str} {dateTime}");
 
 
             #endregion
 
             #region parsing
-            //int num =int.Parse(Console.ReadLine());
-            //Console.WriteLine( num);
-            //double dNum = double.Parse(Console.ReadLine());
-            //Console.WriteLine( dNum);
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine(num);
+            double dNum = double.Parse(Console.ReadLine());
+            Console.WriteLine(dNum);
 
             #endregion
 
-            //double num1 = double.Parse(Console.ReadLine()); 
-            //double num2 = double.Parse(Console.ReadLine());
-            //double res = num1 % num2;
-            //Console.WriteLine($"{res} ");
+            double num1 = double.Parse(Console.ReadLine());
+            double num2 = double.Parse(Console.ReadLine());
+            double res = num1 % num2;
+            Console.WriteLine($"{res} ");
 
-            //0.2
+          //  0.2
 
 
 
             #region math and logical
 
-            //math + - / % 
+          //  math + - / %
 
 
-            //logical , >, < , == < ,<= ,>= , != ,|| , &&
+         //   logical , >, < , == < ,<= ,>= , != ,|| , &&
 
-            //var v1 = "a";
-            //var v2 = 3 > 3;
-            //Console.WriteLine("");
+            var v1 = "a";
+            var v2 = 3 > 3;
+            Console.WriteLine("");
 
             #endregion
 
             #region prefix and sufix
-            // ++ --
+         //   ++--
 
 
 
             #endregion
 
             #region IF
-            // if => age
-            //int age = 18;
-            //if (age == 18)
-            //{
-            //    Console.WriteLine("u can make licance");
-            //}
-            //if (age > 18)
-            //{
-            //    Console.WriteLine(">18");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("u can make licance");
+           //  if => age
+            int age = 18;
+            if (age == 18)
+            {
+                Console.WriteLine("u can make licance");
+            }
+            if (age > 18)
+            {
+                Console.WriteLine(">18");
+            }
+            else
+            {
+                Console.WriteLine("u can make licance");
 
-            //}
+            }
 
 
 
@@ -87,47 +87,47 @@ namespace Learn_Sharp
             #endregion
 
             #region Loops
-            // for , while , foreach => 0 : n
-            //for (int i = 0; i < 10; i++)
-            //{
-            //    Console.WriteLine(i);
-            //}
-            //int cnt = 0;
-            //while (true)
-            //{
-            //    if (cnt==10)
-            //    {
-            //        break;
-            //    }
-            //Console.WriteLine("Hallo Ahmed");
-            //    cnt++;
-            //}
+          //  for , while , foreach => 0 : n
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            int cnt = 0;
+            while (true)
+            {
+                if (cnt == 10)
+                {
+                    break;
+                }
+                Console.WriteLine("Hallo Ahmed");
+                cnt++;
+            }
 
 
             #endregion
 
             #region Array
-            // arr
-            //string name1 = "Ahmed";
-            //string name2 = "Afaf";
-            //string name3 = "rwan";
+         //   arr
+            string name1 = "Ahmed";
+            string name2 = "Afaf";
+            string name3 = "rwan";
 
-            //string[] names = new string[5];
-            //names[0] = name1;
-            //names[1] = name2;
-            //names[2] = name3;
-            //names[3] = "Ahd";
-            //names[4] = "haron";
+            string[] names = new string[5];
+            names[0] = name1;
+            names[1] = name2;
+            names[2] = name3;
+            names[3] = "Ahd";
+            names[4] = "haron";
 
-            //Console.WriteLine(names);
-            //Console.WriteLine(names[0]);
-            //Console.WriteLine(names[1]);
-            //Console.WriteLine(names[2]);
+            Console.WriteLine(names);
+            Console.WriteLine(names[0]);
+            Console.WriteLine(names[1]);
+            Console.WriteLine(names[2]);
 
-            //foreach(string name in names)
-            //{
-            //    Console.WriteLine(name);
-            //}
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
 
 
 
@@ -135,10 +135,12 @@ namespace Learn_Sharp
             #endregion
 
             #region List & genrics
-            //genrics
+          //  genrics
 
             #endregion
 
+
+            //try hard
 
         }
     }
