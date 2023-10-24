76bbd63813cf9d093ab362215f2f4c3aaf92e0dd
Abdo Elbessa <zezoelbessa10@egyeagles.com>
2023-10-24 21:01:46 +0300

    Add project files.

---
+++ Learn C#/Program.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.VisualBasic;
+using System.Data;
+
+namespace Learn_Sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region var
+            // int , string , double , datatime , bool , char
+            //int x = 0;
+            //string str = Console.ReadLine();
+            //double  d = 15555555555.22222d;
+            //DateTime dateTime = DateTime.Now;
+            //bool t = true;
+            //bool f = false;
+            //char ch = 'a';
+
+            #endregion
+
+            #region console
+            // cw , cr
+
+            //Console.WriteLine($"Hello! {str} {dateTime}");
+
+
+            #endregion
+
+            #region parsing
+            //int num =int.Parse(Console.ReadLine());
+            //Console.WriteLine( num);
+            //double dNum = double.Parse(Console.ReadLine());
+            //Console.WriteLine( dNum);
+
+            #endregion
+
+            //double num1 = double.Parse(Console.ReadLine()); 
+            //double num2 = double.Parse(Console.ReadLine());
+            //double res = num1 % num2;
+            //Console.WriteLine($"{res} ");
+
+            //0.2
+
+
+
+            #region math and logical
+
+            //math + - / % 
+
+
+            //logical , >, < , == < ,<= ,>= , != ,|| , &&
+
+            //var v1 = "a";
+            //var v2 = 3 > 3;
+            //Console.WriteLine("");
+
+            #endregion
+
+            #region prefix and sufix
+            // ++ --
+
+
+
+            #endregion
+
+            #region IF
+            // if => age
+            //int age = 18;
+            //if (age == 18)
+            //{
+            //    Console.WriteLine("u can make licance");
+            //}
+            //if (age > 18)
+            //{
+            //    Console.WriteLine(">18");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("u can make licance");
+
+            //}
+
+
+
+
+            #endregion
+
+            #region Loops
+            // for , while , foreach => 0 : n
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //int cnt = 0;
+            //while (true)
+            //{
+            //    if (cnt==10)
+            //    {
+            //        break;
+            //    }
+            //Console.WriteLine("Hallo Ahmed");
+            //    cnt++;
+            //}
+
+
+            #endregion
+
+            #region Array
+            // arr
+            //string name1 = "Ahmed";
+            //string name2 = "Afaf";
+            //string name3 = "rwan";
+
+            //string[] names = new string[5];
+            //names[0] = name1;
+            //names[1] = name2;
+            //names[2] = name3;
+            //names[3] = "Ahd";
+            //names[4] = "haron";
+
+            //Console.WriteLine(names);
+            //Console.WriteLine(names[0]);
+            //Console.WriteLine(names[1]);
+            //Console.WriteLine(names[2]);
+
+            //foreach(string name in names)
+            //{
+            //    Console.WriteLine(name);
+            //}
+
+
+
+
+            #endregion
+
+            #region List & genrics
+            //genrics
+
+            #endregion
+
+
+        }
+    }
+}
